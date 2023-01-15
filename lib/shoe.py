3c4f392ab90e883e588ce5f2eb973275f730e909
Elijah Smith <elijahsmith.m@gmail.com>
2023-01-14 23:10:19 -0600

    oop putting it all together

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self.size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if type(size) is int:
+            self._size = size
+
+    size = property(get_size, set_size)
